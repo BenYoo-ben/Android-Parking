28e78bdca1ee6a36494482e6bdcddb776a0bf995
BenYoo-ben <neverknown0909@gmail.com>
2019-11-12 21:15:21 +0900

    20191112

---
+++ FirstUnityProject/Ruby/RubyAdventure/Assets/Scripts/RubyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rigidbody2d;
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 25;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Debug.Log(horizontal);
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
+        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
+    }
+}
